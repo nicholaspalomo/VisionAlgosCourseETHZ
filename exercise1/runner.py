b077e49c7813674aa5dda2f19248610025b21f81
Nico Palomo <nico@sunflower-labs.com>
2020-09-27 16:21:47 +0200

    cleanup, still need to finish part 3 of exercise 1

--- exercise1/runner.py
+++ exercise1/runner.py
@@ -18,77 +18,80 @@ def main():
     camera_poses = text_file_handler.read_file(current_file_path + "/poses.txt")
 
     perspective_projection = PerspectiveProjection(camera_K_matrix, camera_D_matrix)
-    perspective_projection.load_image(current_file_path + "/images_undistorted/img_0001.jpg")
 
     # Exercise 1, 2.2
-    # project the world origin to the camera/image frame
-    delta = 0.04 # cm spacing between corners
+    def grid():
+        corners_x, corners_y = np.meshgrid(np.linspace(0, 8, 9), np.linspace(0, 5, 6))
 
-    def grid(camera_pose, image_distortion=False):
-        point_in_W = np.zeros((3,1))
-        corners_x, corners_y = np.meshgrid(np.linspace(0, 8 * delta, 9), np.linspace(0, 5 * delta, 6))
-        corners_in_C = np.zeros((corners_x.shape[0]*corners_x.shape[1], 2))
-        k = 0
-        for i in range(corners_x.shape[0]):
-            for j in range(corners_x.shape[1]):
-                point_in_W[0] = corners_x[i, j]
-                point_in_W[1] = corners_y[i, j]
-                if image_distortion:
-                    point_in_C, _ = perspective_projection.project_W_to_C_distortion(camera_pose, point_in_W)
-                else:
-                    point_in_C, _ = perspective_projection.project_W_to_C(camera_pose, point_in_W)
-                corners_in_C[k, :] = np.transpose(point_in_C)
-                k += 1
+        corners_in_W = np.zeros((corners_x.shape[0]*corners_x.shape[1], 3))
+        corners_in_W[:,0] = np.reshape(corners_x, (corners_in_W.shape[0], 1)).squeeze(axis=1)
+        corners_in_W[:,1] = np.reshape(corners_y, (corners_in_W.shape[0], 1)).squeeze(axis=1)
 
-        return corners_in_C
+        return corners_in_W * 0.04
 
     plt.figure()
-    perspective_projection.line(grid, camera_poses[0,:], linestyle='o', image_distortion=False)
-    perspective_projection.display_image()
+    perspective_projection.draw_line_in_I(
+        current_file_path + "/images_undistorted/img_0001.jpg", 
+        grid(), 
+        camera_poses[0,:],
+        linestyle='o',
+        image_distortion=False)
     plt.pause(3)
     plt.close()
 
     # Exercise 1, 2.3
-    def get_cube_corners_in_C(camera_pose, image_distortion=False):
-        point_in_W = np.zeros((3,1))
+    def get_cube_corners_in_W():
         edge_length = 2.
         corners_x = 4. + edge_length * np.array([0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0])
         corners_y = 1. + edge_length * np.array([0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1])
         corners_z = 0. - edge_length * np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1])
-        corners_in_C = np.zeros((len(corners_x), 2))
-        m = 0
-        for i, j, k in zip(corners_x, corners_y, corners_z):
-            point_in_W[0], point_in_W[1], point_in_W[2] = i, j, k
-            point_in_W *= delta
-            if image_distortion:
-                point_in_C, _ = perspective_projection.project_W_to_C_distortion(camera_pose, point_in_W)
-            else:
-                point_in_C, _ = perspective_projection.project_W_to_C(camera_pose, point_in_W)
-            corners_in_C[m, :] = np.transpose(point_in_C)
-            m += 1
 
-        return corners_in_C
+        corners_in_W = np.zeros((corners_x.shape[0], 3))
+        corners_in_W[:,0] = corners_x
+        corners_in_W[:,1] = corners_y
+        corners_in_W[:,2] = corners_z
+
+        return corners_in_W * 0.04
 
     plt.figure()
-    perspective_projection.line(get_cube_corners_in_C, camera_poses[0,:], linestyle='-o', image_distortion=False)
+    perspective_projection.draw_line_in_I(
+        current_file_path + "/images_undistorted/img_0001.jpg",
+        get_cube_corners_in_W(),
+        camera_poses[0,:],
+        linestyle='-o', 
+        image_distortion=False)
     plt.pause(3)
     plt.close()
 
     # Exercise 1, 2.4
-    plt.figure()
-    perspective_projection.animation(current_file_path + "/images", get_cube_corners_in_C, camera_poses, linestyle='-o', image_distortion=False)
-    plt.close()
+    # plt.figure()
+    # perspective_projection.animation_with_drawing_in_I(
+    #     current_file_path + "/images",
+    #     get_cube_corners_in_W(),
+    #     camera_poses,
+    #     linestyle='-o', 
+    #     image_distortion=False)
+    # plt.close()
 
     # Exercise 1, 3.1-2
-    perspective_projection.load_image(current_file_path + "/images/img_0001.jpg")
     plt.figure()
-    perspective_projection.line(grid, camera_poses[0,:], linestyle='o', image_distortion=True)
+    perspective_projection.draw_line_in_I(
+        current_file_path + "/images/img_0001.jpg",
+        get_cube_corners_in_W(),
+        camera_poses[0,:],
+        linestyle='-o',
+        image_distortion=True)
     perspective_projection.display_image()
-    plt.pause(5)
+    plt.pause(3)
     plt.close()
 
     plt.figure()
-    perspective_projection.animation(current_file_path + "/images", get_cube_corners_in_C, camera_poses, linestyle='-o', image_distortion=True)
+    perspective_projection.animation_with_drawing_in_I(
+        current_file_path + "/images",
+        get_cube_corners_in_W(),
+        camera_poses,
+        linestyle='-o', 
+        image_distortion=True)
     plt.close()
 
     # Exercise 1, 3.3
--- exercise1/tools/algo/perspective_projection.py
+++ exercise1/tools/algo/perspective_projection.py
@@ -36,55 +36,86 @@ class PerspectiveProjection:
 
         return
 
-    def animation(self, images_dir, function_handle, camera_poses, linestyle='o', decimation=0.001, image_distortion=False):
+    def animation_with_drawing_in_I(self, image_path, points_in_W, camera_poses, linestyle='o', decimation=0.001, image_distortion=False):
 
         i = 0
-        for frame in sorted(os.listdir(images_dir)):
-            self.load_image(images_dir + "/" + frame)
-            self.line(function_handle, camera_poses[i,:], linestyle=linestyle, image_distortion=image_distortion)
-            self.display_image()
+        for frame in sorted(os.listdir(image_path)):
+            self.draw_line_in_I(image_path + "/" + frame, points_in_W, camera_poses[i,:], linestyle=linestyle, image_distortion=image_distortion)
             plt.pause(decimation)
             plt.clf()
             i += 1
 
         return
 
-    def line(self, function_handle, camera_pose, linestyle='o', image_distortion=False):
+    def draw_line_in_I(self, image_path, points_in_W, camera_pose, linestyle='o', image_distortion=False):
+        if points_in_W.shape[1] > 3:
+            points_in_W = np.transpose(points_in_W)
 
-        points = function_handle(camera_pose, image_distortion)
-        plt.plot(points[:,0], points[:,1], linestyle)
+        self.load_image(image_path)
+
+        plot_points_in_I = np.zeros((points_in_W.shape[0], 2))
+        i = 0
+        for j in range(points_in_W.shape[0]):
+            points_in_I, _ = self.project_W_to_I(camera_pose, np.reshape(points_in_W[j,:], (3, 1)), image_distortion=image_distortion)
+            plot_points_in_I[i, :] = np.transpose(points_in_I)
+            i += 1
+
+        plt.plot(plot_points_in_I[:,0], plot_points_in_I[:,1], linestyle)
+
         self.display_image()
 
         return
 
-    def project_W_to_C(self, camera_pose, point_in_W):
+    def project_W_to_I(self, camera_pose, point_in_W, image_distortion=False):
+
+        point_in_C = self.project_W_to_C(camera_pose, point_in_W, image_distortion=image_distortion)
+
+        pixel_coordinates, lam = self.project_C_to_I(point_in_C, image_distortion=image_distortion)
+
+        return pixel_coordinates, lam # [u, v], lambda
+
+    def project_W_to_C(self, camera_pose, point_in_W, image_distortion=False):
         _, _, transform_mat_W_to_C = self.get_transform_mat(camera_pose)
+
         point_in_W = np.reshape(point_in_W, (3,1))
+        point_in_W = np.concatenate((point_in_W, np.ones((1,1))), axis=0)
 
-        point_in_C = np.matmul(self.camera_K_matrix_, transform_mat_W_to_C)
-        point_in_C = np.matmul(point_in_C, np.concatenate((point_in_W, np.ones((1,1))), axis=0))
+        point_in_C = np.matmul(transform_mat_W_to_C, point_in_W)
 
-        return point_in_C[:2] / point_in_C[2], point_in_C[2] # [u, v], lambda
+        if image_distortion:
+            point_in_C_normalized = point_in_C / point_in_C[2]
 
-    def project_W_to_C_distortion(self, camera_pose, point_in_W):
-        # Project point from frame W to frame C
-        _, _, transform_mat_W_to_C = self.get_transform_mat(camera_pose)
-        point_in_C = np.matmul(transform_mat_W_to_C, np.concatenate((point_in_W, np.ones((1,1))), axis=0))
+            k1, k2 = self.camera_D_matrix_[0], self.camera_D_matrix_[1]
+            r = np.linalg.norm(point_in_C_normalized[:2])
 
-        # Project to image plane to get normalized coordinates
-        point_in_C_normalized = point_in_C / point_in_C[2]
+            # Apply lens distortion model
+            point_in_C = point_in_C_normalized
+            point_in_C[:2] = (1 + k1 * r**2 + k2 * r**4) * point_in_C_normalized[:2]
 
-        k1, k2 = self.camera_D_matrix_[0], self.camera_D_matrix_[1]
-        r = np.linalg.norm(point_in_C_normalized[:2])
+        return point_in_C
 
-        # Apply lens distortion model
-        point_in_C_distorted = point_in_C_normalized
-        point_in_C_distorted[:2] = (1 + k1 * r**2 + k2 * r**4) * point_in_C_normalized[:2]
+    def project_C_to_I(self, point_in_C, image_distortion=False):
 
-        # convert distorted normalized coordinates to get discretized pixel coordinates, [u, v]
-        pixel_coordinates = np.matmul(self.camera_K_matrix_, point_in_C_distorted * point_in_C[2])
+        pixel_coordinates = np.matmul(self.camera_K_matrix_, point_in_C)
+        if image_distortion:
+            pixel_coordinates *= point_in_C[2]
 
-        return pixel_coordinates[:2] / pixel_coordinates[2], pixel_coordinates[2]
+        return pixel_coordinates[:2] / pixel_coordinates[2], point_in_C[2] # [u, v], lambda
+
+    def undistort_image(self, image_path):
+        self.load_image(image_path)
+
+        K_inv = np.linalg.inv(self.camera_K_matrix_)
+
+        # k1, k2 = self.camera_D_matrix_[0], self.camera_D_matrix_[1]
+        # r = np.linalg.norm(point_in_C_normalized[:2])
+        # distortion_coeff_inv = 1 / (1 + )
+
+        # create meshgrid of the points
+
+        # map the points
+
+        return
 
     def get_transform_mat(self, camera_pose):
         rot_mat_W_to_C = self.angle_axis_2_rot_mat(camera_pose[:3])
@@ -98,11 +129,7 @@ class PerspectiveProjection:
         theta = np.linalg.norm(angle_axis)
         k = angle_axis / theta
         
-        k_skew_symmetrix = np.zeros((3,3))
-        k_skew_symmetrix[0, 1] = -k[2]
-        k_skew_symmetrix[0, 2] = k[1]
-        k_skew_symmetrix[1, 2] = -k[0]
-        k_skew_symmetrix -= np.transpose(k_skew_symmetrix)
+        k_skew_symmetrix = PerspectiveProjection.skew_symmetric_3(k)
 
         # Apply Rodrigues formula to get the unnormalized rotation matrix from the angle axis representation
         rot_mat = eye3 + math.sin(theta) * k_skew_symmetrix + (1 - math.cos(theta)) * k_skew_symmetrix * k_skew_symmetrix
@@ -112,3 +139,13 @@ class PerspectiveProjection:
         rot_mat = np.matmul(U, V_transpose)
 
         return rot_mat
+
+    @staticmethod
+    def skew_symmetric_3(vec):
+        skew_symmetrix = np.zeros((3,3))
+        skew_symmetrix[0, 1] = -vec[2]
+        skew_symmetrix[0, 2] = vec[1]
+        skew_symmetrix[1, 2] = -vec[0]
+        skew_symmetrix -= np.transpose(skew_symmetrix)
+
+        return skew_symmetrix
