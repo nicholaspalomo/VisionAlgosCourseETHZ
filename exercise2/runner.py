cdfdc9f466532865b8e41f6a1a5ce65587fbedef
Nico Palomo <nico@sunflower-labs.com>
2020-10-03 18:24:14 +0200

    initial commit

---
+++ exercise2/runner.py
@@ -0,0 +1,18 @@
+import cv2 # OpenCV
+import matplotlib as mtplb
+import numpy as np
+import argparse as ap
+import pathlib
+from matplotlib import pyplot as plt
+import os
+import sys
+sys.path.append(".")
+
+from tools.utils.process_text_file import ProcessTextFile
+from tools.algo.perspective_projection import PerspectiveProjection
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
