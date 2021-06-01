bb546175ce526b5eff5fb97e8b1bf4f8bc028dc0
Nico Palomo <nico@sunflower-labs.com>
2021-06-01 08:35:50 +0200

    initial commit

---
+++ exercise5/code/disparityToPointCloud.m
@@ -0,0 +1,6 @@
+function [points, intensities] = disparityToPointCloud(...
+    disp_img, K, baseline, left_img)
+% points should be 3xN and intensities 1xN, where N is the amount of pixels
+% which have a valid disparity. I.e., only return points and intensities
+% for pixels of left_img which have a valid disparity estimate! The i-th
+% intensity should correspond to the i-th point.
---
+++ exercise5/code/getDisparity.m
@@ -0,0 +1,7 @@
+function disp_img = getDisparity(...
+    left_img, right_img, patch_radius, min_disp, max_disp)
+% left_img and right_img are both H x W and you should return a H x W
+% matrix containing the disparity d for each pixel of left_img. Set
+% disp_img to 0 for pixels where the SSD and/or d is not defined, and for d
+% estimates rejected in Part 2. patch_radius specifies the SSD patch and
+% each valid d should satisfy min_disp <= d <= max_disp.
